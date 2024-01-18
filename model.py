Decoder normalization fix

model.py
@@ -47,7 +47,8 @@ class EchoMorphParameters:
 
 
 class AudioCoder(nn.Module):
-    def __init__(self, spect_width, hidden_dim_m, heads, spect_len, drop, blocks_num, cross_n, mid_repeat_interval):
+    def __init__(self, spect_width, hidden_dim_m, heads, spect_len, drop, blocks_num, cross_n, mid_repeat_interval,
+                 do_last_layers_norm=True):
         super().__init__()
         assert all([x % 2 == 0 for x in blocks_num]), "Criss-crossing won't work"
 
@@ -55,13 +56,15 @@ class AudioCoder(nn.Module):
         for i in range(sum(blocks_num)):
             ed = spect_width if i % 2 == 0 else spect_len
             this_cross_n = cross_n if i % 2 == 0 else 0
+            dn = do_last_layers_norm or i < sum(blocks_num) - 2
             blocks += [TransformerBlock(
                 embed_dim=ed,
                 num_heads=heads,
                 hidden_dim=ed * hidden_dim_m,
                 attn_drop=drop,
                 drop=drop,
-                n_cross_attn_blocks=this_cross_n
+                n_cross_attn_blocks=this_cross_n,
+                do_norm=dn
             )]
         self.blocks_pre = nn.ModuleList(blocks[:blocks_num[0]])
         self.blocks_mid = nn.ModuleList(blocks[blocks_num[0]:blocks_num[1]])
@@ -119,8 +122,11 @@ class AudioEncoder(AudioCoder):
 
 class AudioDecoder(AudioCoder):
     def __init__(self, pars: EchoMorphParameters):
+        # If we expect the last block to output a spectrogram,
+        # it is not a sensible thing to do to add normalization layers to it.
+        # Also one block before it, just in case.
         super().__init__(pars.spect_width, pars.ad_hidden_dim_m, pars.ad_heads, pars.fragment_len,
-                         pars.drop, pars.ad_blocks, 2, pars.mid_repeat_interval)
+                         pars.drop, pars.ad_blocks, 2, pars.mid_repeat_interval, do_last_layers_norm=False)
 
     def forward(self, x: Tensor, speaker_characteristic: Tensor, history: Tensor) -> Tensor:
         x = super().forward(x, [speaker_characteristic, history])

transformer_blocks.py
@@ -108,7 +108,8 @@ class TransformerBlock(nn.Module):
         hidden_dim: int,
         attn_drop: float,
         drop: float,
-        n_cross_attn_blocks: int = 0
+        n_cross_attn_blocks: int = 0,
+        do_norm: bool = True
     ):
         super().__init__()
         self.self_attn = SelfAttention(embed_dim, num_heads, attn_drop)
@@ -123,7 +124,7 @@ class TransformerBlock(nn.Module):
 
         self.ffn = FeedForward(embed_dim, hidden_dim)
         self.ffn_dropout = nn.Dropout(p=drop)
-        self.ffn_norm = nn.LayerNorm(embed_dim)
+        self.ffn_norm = nn.LayerNorm(embed_dim) if do_norm else None
 
     def forward(self, layer_input: T, cross_attn_inputs: [T] = []) -> T:
         res = layer_input
@@ -139,6 +140,7 @@ class TransformerBlock(nn.Module):
         res = out
         out = self.ffn(out)
         out = self.ffn_dropout(out)
-        out = self.ffn_norm(out + res)
+        if self.ffn_norm is not None:
+            out = self.ffn_norm(out + res)
 
         return out