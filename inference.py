fd7a169e3672125051874c58fe564227b9db697e
Semjon Kravtsenko <semjon.00@gmail.com>
2024-01-19 22:31:39 +0200

    Massively buffed inference/demo capabilities
    Still no inference/train parameters toggle
    NOt properly tested

--- inference.py
+++ inference.py
@@ -1,8 +1,19 @@
-from audio import AudioConventer
-from model import EchoMorph
 import torch
 import os
 import pathlib
+
+import sys
+
+from audio import AudioConventer
+from model import EchoMorph
+
+def play_audio(filename):
+    import subprocess
+    if sys.platform.startswith('linux'):
+        subprocess.call(['aplay', filename], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    else:
+        print('Playback not implemented...')
+
 
 def crop_from_middle(x, length):
     if x.size(0) < length:
@@ -12,70 +23,106 @@ def crop_from_middle(x, length):
     x = x[start:start + length, ...]
     return x
 
-def standard_inference(model: EchoMorph, target_sample, source):
-    """Trivial inference procedure. No interpolating or using the entire target sample"""
-    speaker_characteristic = model.speaker_encoder(crop_from_middle(target_sample, model.pars.target_sample_len))
 
-    hl = model.pars.history_len
-    fl = model.pars.fragment_len
-    # source_length = len(source)
+class InferenceFreestyle:
+    def __init__(self):
+        # TODO: half mode
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.ac = AudioConventer(device)
 
-    # Padding the source so we have empty history beforehand and some space after the end
-    source = torch.cat((torch.zeros([hl, source.size(1)]), source, torch.zeros([fl, source.size(1)])), dim=0)
-    target = torch.zeros_like(source)
+        root_snapshots = pathlib.Path("snapshots")
+        snapshots = sorted(os.listdir(root_snapshots))
+        if len(snapshots) < 1:
+            print('No model snapshot means no inference is possible.')
+            exit(1)
 
-    print('Inferencing: [', end='')
-    for cur in range(hl, target.size(0), fl):
-        intermediate = model.rando_mask(model.audio_encoder(source[cur:cur+fl, :], source[cur-hl:cur, :]))
-        target[cur:cur+fl, :] = model.audio_decoder(intermediate, speaker_characteristic, target[cur-hl:cur, :])
-        print('.', end='')
-    print(']')
-    print('Done!')
+        directory = root_snapshots / snapshots[-1]
+        print(f'Loading an EchoMorph model stored in {directory}... ', end='')
+        self.model = torch.load(directory / 'model.bin')
+        self.model.eval()
+        print('Done!')
 
-    # Maybe don't actually truncate the last fragment, what if there is useful signal there?
-    return target[hl:, ...]
+        self.bank = {}
+        self.next_num = 10
+
+    def to_bank(self, letter, obj, recipie):
+        """Stores an object that can later be used with Freestyle"""
+        name = f'{letter}{self.next_num}'
+        self.next_num += 1
+        self.bank[name] = (obj, recipie)
+        print(f'{name}: {recipie}')
+        return name
+
+    def load_sample(self, path):
+        wv = self.ac.load_audio(path)
+        sg = self.ac.convert_from_wave(wv)
+        return self.to_bank('s', sg, f'Loaded from {path}')
+
+    def save_sample(self, name, path):
+        audio = self.ac.convert_to_wave(self.bank[name][0])
+        self.ac.save_audio(audio, path)
+
+    def play_sample(self, name):
+        import tempfile
+        with tempfile.NamedTemporaryFile(suffix=".wav") as tmpfile:
+            self.save_sample(name, tmpfile.name)
+            play_audio(tmpfile.name)
+
+    def derive_sc(self, name):
+        with torch.no_grad():
+            print('Deriving: [', end='')
+            speaker_characteristic = self.model.speaker_encoder(
+                crop_from_middle(self.bank[name][0], self.model.pars.target_sample_len)
+            )
+            print('.] Done!')
+            return self.to_bank('c', speaker_characteristic, f'Derived directly from {name}')
+
+    def merge_sc(self, name1, name2, proportion=None):
+        if proportion is None:
+            proportion = 0.5
+        else:
+            proportion = int(proportion)
+        obj = self.bank[name1][0] * (1.0 - proportion) + self.bank[name2][0]
+        recipie = f'Merged from {name1} ({(1.0 - proportion):.3f}) and {name2} {proportion:.3f}'
+        self.to_bank('c', obj, recipie)
+
+    def randomize_sc(self, name, p):
+        obj = self.bank[name][0]
+        o_max, o_min = obj.max(), obj.min()
+        obj += torch.randn_like(obj) * p
+        torch.clamp(obj, min=o_min, max=o_max)
+        self.to_bank('c', obj, f'Forged from {name} by applying {p:.3f} grams of pure chaos')
+
+    def infer(self, sc_name, source_name):
+        hl = self.model.pars.history_len
+        fl = self.model.pars.fragment_len
+
+        sc = self.bank[sc_name][0]
+
+        source = self.bank[source_name][0]
+        source = torch.cat((torch.zeros([hl, source.size(1)]), source, torch.zeros([fl, source.size(1)])), dim=0)
+        target = torch.zeros_like(source)
+
+        with torch.no_grad():
+            print('Inferencing: [', end='')
+            for cur in range(hl, target.size(0), fl):
+                intermediate = self.model.audio_encoder(source[cur:cur + fl, :], source[cur - hl:cur, :])
+                intermediate = self.model.rando_mask(intermediate)
+                target[cur:cur + fl, :] = self.model.audio_decoder(intermediate, sc, target[cur - hl:cur, :])
+                print('.', end='')
+            print('] Done!')
+
+        target = target[hl:, ...]
+        return self.to_bank('s', target, f'Inferenced from {source_name} by {sc_name}')
 
 # TODO: set eval parameters before launching the model (mid blocks repeat, randomask values)
 
-# TODO: allow replacing the target_sample with some nonsense:
-#        - Mix two (or more) speaker representations
-#        - Allow interpolation across different speaker representations (time-axis)
-#        - Add some noise to the representation
-#        - Different types of randomized representations
-#       it screams class and inheritance (SpeakerRepresentationProvider -style)
+# TODO: Allow interpolation across different speaker representations (time-axis)
+#       More types of randomized representations
 
 # TODO: feeding fake history
 
-def inference_base(sample_path, source_path, save_path):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    ac = AudioConventer(device)
-    target_sample = ac.load_audio(sample_path)
-    source = ac.load_audio(source_path)
-    ts_x = ac.convert_from_wave(target_sample)
-    src_x = ac.convert_from_wave(source)
-
-    root_snapshots = pathlib.Path("snapshots")
-    snapshots = sorted(os.listdir(root_snapshots))
-    if len(snapshots) < 1:
-        print('No model snapshot means no inference is possible.')
-        exit(1)
-
-    directory = root_snapshots / snapshots[-1]
-    print(f'  Loading an EchoMorph model stored in {directory}...')
-
-    with torch.no_grad():
-        model = torch.load(directory / 'model.bin')
-        model.eval()
-        output = standard_inference(model, ts_x, src_x)
-        # output = ts_x
-
-        audio = ac.convert_to_wave(output)
-        ac.save_audio(audio, './dataset/result_temp.wav')
-
-
-if __name__ == '__main__':
-    print('=== EchoMorph inference demo ===')
-
+def demo(freestyle: InferenceFreestyle):
     src = input('Speech file path: ')
     if len(src) < 1:
         src = './dataset/tests/example1.mp3'
@@ -85,4 +132,53 @@ if __name__ == '__main__':
         tgt_s = input('Speaker file path: ')
         save = input('Save into: ')
 
-    inference_base(src, tgt_s, save)
+    tgt_s_n = freestyle.load_sample(tgt_s)
+    sc = freestyle.derive_sc(tgt_s_n)
+    src = freestyle.load_sample(src)
+    out = freestyle.infer(sc, src)
+    freestyle.save_sample(out, save)
+    freestyle.play_sample(out)
+
+
+if __name__ == '__main__':
+    print('=== EchoMorph inference demo ===')
+    freestyle = InferenceFreestyle()
+
+    while True:
+        try:
+            cmd = input('> ').split(' ')
+            match cmd[0]:
+                case 'demo':
+                    demo(freestyle)
+                case 'playtest':
+                    s = freestyle.load_sample('./dataset/tests/example1.mp3')
+                    freestyle.play_sample(s)
+                case 'load':
+                    # TODO sc
+                    freestyle.load_sample(cmd[1])
+                case 'save':
+                    # TODO: sc
+                    freestyle.save_sample(cmd[1], cmd[2])
+                case 'derive':
+                    freestyle.derive_sc(cmd[1])
+                case 'merge':
+                    freestyle.merge_sc(*cmd[1:])
+                case 'randomize':
+                    freestyle.randomize_sc(cmd[1], float(cmd[2]))
+                case 'infer':
+                    # TODO: sloppy syntax - use sample as characteristic (derive beforehand)
+                    # TODO: sloppy syntax - swap arguments, figure out by first letter
+                    freestyle.infer(cmd[1], cmd[2])
+                case 'list':
+                    raise NotImplementedError()
+                case 'clear':
+                    raise NotImplementedError()
+                case _:
+                    print('Commands: load save derive'
+                          '          merge randomize infer'
+                          '          list clear')
+        except KeyError as e:
+            print(f'No such object {e.args[0]}')
+        except:
+            import traceback
+            traceback.print_exc()
