4de6fdb282c3e2508c9c4a8a317c5601cda2b3d4
Semjon Kravtsenko <semjon.00@gmail.com>
2024-01-20 13:12:15 +0200

    Inference: lerping, ir radiation, new commands (play, list, clear), merge_sc bugfix

--- inference.py
+++ inference.py
@@ -9,6 +9,7 @@ from model import EchoMorph, EchoMorphParameters
 
 
 def play_audio(filename):
+    # Install alsa-utils / alsa-tools if this does not work
     import subprocess
     if sys.platform.startswith('linux'):
         subprocess.call(['aplay', filename], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
@@ -39,7 +40,7 @@ class InferenceFreestyle:
 
         directory = root_snapshots / sorted(os.listdir(root_snapshots))[-1]
         print(f'Loading an EchoMorph model stored in {directory}... ', end='')
-        training_parameters = EchoMorphParameters()  #TODO: inference parameters
+        training_parameters = EchoMorphParameters()
         self.model = EchoMorph(training_parameters)
         self.model.load_state_dict(torch.load(directory / 'model.bin'))
         self.model.eval()
@@ -62,7 +63,7 @@ class InferenceFreestyle:
             sg = self.ac.convert_from_wave(wv)
             return self.to_bank('s', sg, f'Loaded from {path}')
         elif path.split('.')[-1] in ['emc']:
-            sc = pickle.load(open(path, 'rb'))
+            sc = torch.load(path, weights_only=True)
             return self.to_bank('c', sc, f'Loaded from {path}')
         else:
             raise NotImplementedError()
@@ -76,7 +77,7 @@ class InferenceFreestyle:
         elif name[0] == 'c':
             if path.split('.')[-1] not in ['emc']:
                 path += '.emc'
-            pickle.dump(self.bank[name][0], open(path, 'wb'))
+            torch.save(self.bank[name][0], path)
         else:
             raise NotImplementedError()
 
@@ -96,12 +97,17 @@ class InferenceFreestyle:
             return self.to_bank('c', speaker_characteristic, f'Derived directly from {name}')
 
     def merge_sc(self, name1, name2, proportion=None):
+        assert 0.0 <= proportion
         if proportion is None:
             proportion = 0.5
         else:
             proportion = int(proportion)
-        obj = self.bank[name1][0] * (1.0 - proportion) + self.bank[name2][0]
-        recipie = f'Merged from {name1} ({(1.0 - proportion):.3f}) and {name2} {proportion:.3f}'
+        obj = self.bank[name1][0] * (1.0 - proportion) + self.bank[name2][0] * proportion
+        if proportion <= 1.0:
+            recipie = f'Merged from {name1} ({(1.0 - proportion):.3f}) and {name2} {proportion:.3f}'
+        else:
+            recipie = f'Overcharged from {name2} (x{proportion:.3f}) by ' \
+                      f'un-tainting {name2} ({(1.0 - proportion):.3f}) from it'
         self.to_bank('c', obj, recipie)
 
     def randomize_sc(self, name, p):
@@ -111,41 +117,57 @@ class InferenceFreestyle:
         torch.clamp(obj, min=o_min, max=o_max)
         self.to_bank('c', obj, f'Forged from {name} by applying {p:.3f} grams of pure chaos')
 
-    def infer(self, sc_name, source_name, tradeoff: float = 0.5, quality: int = 10):
+    def infer(self, sc_name, source_name, tradeoff: float = 0.5, quality: int = 10, radiation: float = 0):
         # Updating model settings
         assert 0 <= quality
         for mp in [self.model.audio_encoder, self.model.audio_decoder]:
             mp.set_mid_repeat_interval(quality)
-        assert 0.0 <= tradeoff <= 1.0
         self.model.rando_mask.set_p(tradeoff)
+
+        do_lerp = '->' in sc_name
+        if do_lerp:
+            sc = [self.bank[x][0] for x in sc_name.split('->')]
+        else:
+            sc = self.bank[sc_name][0]
+        source = self.bank[source_name][0]
 
         hl = self.model.pars.history_len
         fl = self.model.pars.fragment_len
-
-        sc = self.bank[sc_name][0]
-
-        source = self.bank[source_name][0]
         source = torch.cat((torch.zeros([hl, source.size(1)]), source, torch.zeros([fl, source.size(1)])), dim=0)
         target = torch.zeros_like(source)
-
         with torch.no_grad():
             print('Inferencing: [', end='')
             for cur in range(hl, target.size(0), fl):
+                if do_lerp:
+                    lerp_c = (cur - hl) / (target.size(0) - hl)
+                    cur_sc = sc[0] * (1 - lerp_c) + lerp_c * sc[1]
+                else:
+                    cur_sc = sc
                 intermediate = self.model.audio_encoder(source[cur:cur + fl, :], source[cur - hl:cur, :])
+                if radiation > 1e-9:
+                    intermediate += torch.where(intermediate == 0, torch.tensor(0),
+                                                torch.randn_like(intermediate) * radiation)
                 intermediate = self.model.rando_mask(intermediate)
-                target[cur:cur + fl, :] = self.model.audio_decoder(intermediate, sc, target[cur - hl:cur, :])
+                target[cur:cur + fl, :] = self.model.audio_decoder(intermediate, cur_sc, target[cur - hl:cur, :])
                 print('.', end='')
             print('] Done!')
 
         target = target[hl:, ...]
-        return self.to_bank('s', target, f'Inferenced from {source_name} by {sc_name}')
+        return self.to_bank('s', target, f'Inferenced from {source_name} by {sc_name} with '
+                                         f't={tradeoff:.3f}, q={quality}, r={radiation:.3f}')
 
-# TODO: set eval parameters before launching the model (mid blocks repeat, randomask values)
+    def list(self):
+        for f in self.bank.keys():
+            print(f'{f}: {self.bank[f][1]}')
 
-# TODO: Allow interpolation across different speaker representations (time-axis)
-#       More types of randomized representations
+    def clear_bank(self):
+        # TODO: memory cleanup?
+        self.bank = {}
 
-# TODO: feeding fake history
+    # Future freestyling ideas:
+    #     time-domain sc lerping - replace values gradually
+    #     feeding fake history
+
 
 def demo(freestyle: InferenceFreestyle):
     src = input('Speech file path: ')
@@ -189,23 +211,27 @@ if __name__ == '__main__':
                 case 'randomize':
                     freestyle.randomize_sc(cmd[1], float(cmd[2]))
                 case 'infer':
-                    # TODO: sloppy syntax - use sample as characteristic (derive beforehand)
-                    # TODO: sloppy syntax - swap arguments, figure out by first letter
                     mods = {}
                     for mod in cmd[3:]:
                         if mod.startswith('q='):
                             mods['quality'] = int(mod[2:])
                         if mod.startswith('t='):
                             mods['tradeoff'] = float(mod[2:])
+                        if mod.startswith('r='):
+                            mods['radiation'] = float(mod[2:])
+                    if cmd[1][0] == 's' and cmd[2][0] == 'c':
+                        cmd[1], cmd[2] = cmd[2], cmd[1]
                     freestyle.infer(cmd[1], cmd[2], **mods)
+                case 'play':
+                    freestyle.play_sample(cmd[1])
                 case 'list':
-                    raise NotImplementedError()
+                    freestyle.list()
                 case 'clear':
-                    raise NotImplementedError()
+                    freestyle.clear_bank()
                 case _:
                     print('Commands: load save derive'
                           '          merge randomize infer'
-                          '          list clear')
+                          '          play list clear')
         except KeyError as e:
             print(f'No such object {e.args[0]}')
         except:
